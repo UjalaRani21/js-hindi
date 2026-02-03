7a5fc033ee39b7c7cb479f543519160447f128ed
UjalaRani21 <ujala.rani21sep@gmail.com>
2026-02-03 06:00:45 +0000

    datatypes

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,40 @@
+"use strict" ; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs, not browser that's why this will not show any output
+
+// console.log(3+3); console.log("Ujala")
+
+/* agar hume continue code likhna h to ; lagana padega ye line of code end hone ke baad
+  par ye ek aachi practice nhi hai
+  code hamesha readable hona chahiye 
+  isliye humlog aache se likhenge */
+
+  console.log(3+3);
+  console.log("Ujala")
+
+  /*
+  numbers = 2 to power 53
+  bigint (for big numbers)
+  string ('' / ""){prefer to use double quote}
+  boolean (true/false)
+  null (standalone value) {object is the datatype of null}
+  undefined (undefined is the datatype of undefined )
+  symbol (unique)
+
+  these all are primitive (basic data types that store simple values)
+
+  object (this is also a data type)
+  */
+ 
+   let age = 20
+
+  console.log(typeof "Ujala");
+  console.log(typeof age);
+  console.log(typeof null);
+  console.log(typeof undefined);
+  
+  
+  
+  
+
+  
