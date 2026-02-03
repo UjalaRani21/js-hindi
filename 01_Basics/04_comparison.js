dea927d25a68adecbee143f7c8fd1321740a54b0
UjalaRani21 <ujala.rani21sep@gmail.com>
2026-02-03 08:47:26 +0000

    comparison

---
+++ 01_Basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(null>0);
+// console.log(null==0);
+// output is false for both 
+
+// console.log(null>=0); // output is true
+
+// console.log(undefined>0);
+// console.log(undefined<0);
+// console.log(undefined==0);
+
+// is tino ka output false hoga
+
+console.log("2" == 2);
+
+// ===
+console.log("2" === 2);
+console.log(2 === 2);
+
