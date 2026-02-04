6244d8b16fcc0c60320ac3104849d12a6a506710
UjalaRani21 <ujala.rani21sep@gmail.com>
2026-02-04 08:04:35 +0000

    datatypes summary

---
+++ 01_Basics/dataTypes_summary.js
@@ -0,0 +1,30 @@
+const bigNumber = 6485748569828365865374636878468n
+console.log(typeof bigNumber);
+// output is bigint
+
+const id = Symbol('123')
+console.log(typeof id);
+// output is symbol
+
+const cartoons = ["motu", "patlu", "ninja"]
+console.log(typeof cartoons);
+// output is object 
+
+const myFunction = function(){
+    console.log("Hello World")
+}
+console.log(typeof myFunction);
+// output is function
+
+let myObj = {
+    name : "ujala",
+    age : 20,
+}
+console.log(typeof myObj);
+// output is object
+
+const score = 100.8
+console.log(typeof score);
+// output is number 
+
+
